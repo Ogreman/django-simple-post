8ce97dadb36e55b48c34603113f5541d6841e239
Ogreman <theogreman@gmail.com>
2014-01-29 02:01:33 +0000

    Updating

--- forum_thing/settings.py
+++ forum_thing/settings.py
@@ -1,12 +1,3 @@
-"""
-Django settings for lolmoney project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
 
 from unipath import Path
 from django.core.urlresolvers import reverse_lazy
