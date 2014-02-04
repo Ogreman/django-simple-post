ce3114734c71456f80db48d69e4c0d68717d100a
James <james.sixdegrees@gmail.com>
2014-02-04 10:05:44 +0000

    Environment variables.

--- forum_thing/settings.py
+++ forum_thing/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from unipath import Path
 from django.core.urlresolvers import reverse_lazy
 
@@ -90,8 +90,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'theogreman@gmail.com'
-EMAIL_HOST_PASSWORD = 'cwkxgjrfbphxxzaf'
+EMAIL_HOST_USER = os.environ['GMAIL_EMAIL']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
 DEFAULT_FROM_EMAIL = 'notifications@post-thing.com'
 
 # Internationalization
